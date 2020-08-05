5b00a7aee4820d5022e654f0f5a6628f88ac10a8
Stevencoutinho <1484kazuya12@gmail.com>
2020-08-06 01:01:28 +0900

    fullpage

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log('ようこそ');
+new fullpage("#fullpage", {
+  controlArrows: false,
+  scrollingSpeed: 1000,
+  lockAnchors: true,
+  loopHorizontal: false,
+  css3:true,
+  slidesNavigation: true
+});
